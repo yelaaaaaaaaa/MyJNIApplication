06f64317dc93e2d8edad4d4f45d3b35b533e32b3
yela <306431037@qq.com>
2020-07-10 17:59:07 +0800

    commit

--- app/src/main/cpp/ticks/ticks.c
+++ app/src/main/cpp/ticks/ticks.c
@@ -1,12 +1,13 @@
 #include <jni.h>
 #include <pthread.h>
 #include <sys/types.h>
-
+#include <assert.h>
+#include <android/log.h>
 //
 // Created by Administrator on 2020/7/9.
 //
 // Android log function wrappers
-static const char* kTAG = "hello-jniCallback";
+static const char *kTAG = "hello-jniCallback";
 #define LOGI(...) \
   ((void)__android_log_print(ANDROID_LOG_INFO, kTAG, __VA_ARGS__))
 #define LOGW(...) \
@@ -14,33 +15,58 @@ static const char* kTAG = "hello-jniCallback";
 #define LOGE(...) \
   ((void)__android_log_print(ANDROID_LOG_ERROR, kTAG, __VA_ARGS__))
 
-typedef struct tick_cotext{
-    JavaVM  *javaVM;
-    jclass   jniHelperClz;
-    jobject  jniHelperObj;
-    jclass   mainActivityClz;
-    jobject  mainActivityObj;
-    pthread_mutex_t  lock;
-    int      done;
+typedef struct tick_cotext {
+    JavaVM *javaVM;
+    jclass jniHelperClz;
+    jobject jniHelperObj;
+    jclass mainActivityClz;
+    jobject mainActivityObj;
+    pthread_mutex_t lock;
+    int done;
 } TickContext;
 TickContext tickContext;
 
 
 JNIEXPORT void JNICALL
 Java_com_example_echo_TicksActivity_startTicks(JNIEnv *env, jobject thiz) {
-pthread_t threadInfo;
-pthread_attr_t pthreadAttr;
+    pthread_t threadInfo;
+    pthread_attr_t pthreadAttr;
 
-pthread_attr_init(&pthreadAttr);
-pthread_attr_setdetachstate(&pthreadAttr,PTHREAD_CREATE_DETACHED);
-pthread_mutex_init(&tickContext.lock, NULL);
+    pthread_attr_init(&pthreadAttr);
+    pthread_attr_setdetachstate(&pthreadAttr, PTHREAD_CREATE_DETACHED);
+    pthread_mutex_init(&tickContext.lock, NULL);
 
-jclass clz = (*env)->GetObjectClass(env,thiz);
-tickContext.mainActivityClz = (*env)->NewGlobalRef(env,clz);
+    jclass clz = (*env)->GetObjectClass(env, thiz);
+    tickContext.mainActivityClz = (*env)->NewGlobalRef(env, clz);
+    tickContext.mainActivityObj = (*env)->NewGlobalRef(env, thiz);
 
+    int result = pthread_create(&threadInfo, &pthreadAttr, UpdateTicks, &tickContext);
+    assert(result == 0);
+
+    pthread_attr_destroy(&pthreadAttr);
+
+    (void) result;
 }
 
 JNIEXPORT void JNICALL
 Java_com_example_echo_TicksActivity_StopTicks(JNIEnv *env, jobject thiz) {
 
+}
+
+void* UpdateTicks(void *context) {
+    TickContext *pctx = (TickContext *) context;
+    JavaVM *javaVm = pctx->javaVM;
+    JNIEnv *env;
+    jint res =(*javaVm)->GetEnv(javaVm, (void **) &env, JNI_VERSION_1_6);
+
+    if (res != JNI_OK){
+        res = (*javaVm)->AttachCurrentThread(javaVm,&env,NULL);
+        if (res!= JNI_OK){
+          LOGE("Failed to AttachCurrentThread, ErrorCode = %d", res);
+          return NULL;
+        }
+    }
+
+    jmethodID statusId =
+    return context;
 }
